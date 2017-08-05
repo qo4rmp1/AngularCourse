fe05c9ba614d82734fa33ed93cc7fc5559fcdd64
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 10:15:24 +0800

    02 練習設定 routerLink 建立路由連結

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageComponent } from './page/page.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent,
     Page1Component,
     Page2Component
   ],
