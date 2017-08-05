e5cbdae6d0661ac1e026b6f9a7d5ca4c74b21da1
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 10:53:10 +0800

    示範定義獨立的 Route 物件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
+const fallbackRoute = {path: '**', redirectTo: '/', pathMatch: 'full'};
+
 const routes: Routes = [
   {path: '', redirectTo: '/Page1', pathMatch: 'full'},
   {path: 'Page1', component: Page1Component},
   {path: 'Page2', component: Page2Component},
-  {path: '**', redirectTo: '/', pathMatch: 'full'}
+  fallbackRoute
 ];
 
 @NgModule({
