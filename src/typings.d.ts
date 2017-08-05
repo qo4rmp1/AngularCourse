e163120c19065aa9d5d3bb788c3bc857ac453ae8
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 12:13:07 +0800

    06 試圖修復 DashboardComponent 在換頁時無法成功初始化 jQuery 事件的問題-加上註解

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,6 +3,11 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+/*
+因為typescript認不得沒有指定型別的物件
+所以在typings.d.ts
+重新declare 物件型別為any
+*/
 
 declare var $: any;
 declare var Colors;
