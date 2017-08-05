ae10bada8401f1c9493f417aacb489e274538cc9
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 11:53:22 +0800

    05 練習將 WB0901P31 版型加入 Angular 4 專案並套用多頁面路由設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { fallbackRoute } from './shared/fallback-route';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/Page1', pathMatch: 'full'},
-  {path: 'Page1', component: Page1Component},
-  {path: 'Page2', component: Page2Component},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'cards', component: CardsComponent},
   fallbackRoute
 ];
 
