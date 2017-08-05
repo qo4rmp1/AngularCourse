59bc7ae12e370d0b0a0daf2956ac3a53c3c5d393
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 09:42:51 +0800

    01 練習建立 page1 與 page2 元件並設定 Angular 路由進行多頁面顯示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: 'Page1', component: Page1Component},
+  {path: 'Page2', component: Page2Component}
 ];
 
 @NgModule({
