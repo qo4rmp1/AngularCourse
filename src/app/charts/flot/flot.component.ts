f84eb4e05e0b8737e889ad760ea70c7087d124ca
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 13:34:29 +0800

    07 練習設計 /charts/flot 頁面與子路由並修正 jQuery 事件初始化的問題

---
+++ src/app/charts/flot/flot.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FlotCharts } from './init-flotcharts';
+
+@Component({
+  selector: 'app-flot',
+  templateUrl: './flot.component.html',
+  styleUrls: ['./flot.component.css']
+})
+export class FlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    $(FlotCharts);
+
+  }
+
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { initDashboard } from './init-Dashboard';
+import { initDashboard } from './init-dashboard';
 
 @Component({
   selector: 'app-dashboard',
