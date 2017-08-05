f7bdbdf86b41285544d10bce73d20ebec1c6e117
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-05 10:48:55 +0800

    示範預設路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
+  {path: '', component: Page1Component},
   {path: 'Page1', component: Page1Component},
   {path: 'Page2', component: Page2Component}
 ];
